f4eeeabe50367186f0f07014aedede6345251a9e
Bedmor <besim.ozdemir@ogr.sakarya.edu.tr>
2025-10-19 16:13:43 +0300

    changed password

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // Admin password - Change this to your desired password
-const ADMIN_PASSWORD = "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9";
+const ADMIN_PASSWORD = "RmcuU2FrYXJ5YS43Mg==";
 // Current editing product ID
 let editingProductId = null;
 let editingCommentId = null;
